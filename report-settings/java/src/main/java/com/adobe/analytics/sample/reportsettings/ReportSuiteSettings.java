b7a34f60313d22fda0fda24580b7435b6bc87d8c
Tomasz Rękawek <trekawek@gmail.com>
2015-05-12 15:34:26 +0200

    Support for the properties file; display report suite ids.

--- report-settings/java/src/main/java/com/adobe/analytics/sample/reportsettings/ReportSuiteSettings.java
+++ report-settings/java/src/main/java/com/adobe/analytics/sample/reportsettings/ReportSuiteSettings.java
@@ -1,8 +1,13 @@
 package com.adobe.analytics.sample.reportsettings;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 import com.adobe.analytics.client.AnalyticsClient;
+import com.adobe.analytics.client.domain.CompanyReportSuite;
 import com.adobe.analytics.client.domain.Evar;
 import com.adobe.analytics.client.domain.Prop;
 import com.adobe.analytics.client.domain.ReportSuiteEvars;
@@ -11,25 +16,58 @@ import com.adobe.analytics.client.methods.ReportSuiteMethods;
 
 public class ReportSuiteSettings {
 	public static void main(String[] args) throws IOException {
-		final String username = System.getProperty("username");
-		final String password = System.getProperty("password");
-		final String endpoint = System.getProperty("endpoint");
-		final AnalyticsClient client = new AnalyticsClient(username, password, endpoint);
+		final Properties properties = loadProperties();
+		final String username = properties.getProperty("username");
+		final String secret = properties.getProperty("secret");
+		final String endpoint = properties.getProperty("endpoint");
 
+		final AnalyticsClient client = new AnalyticsClient(username, secret, endpoint);
 		final ReportSuiteMethods methods = new ReportSuiteMethods(client);
-		for (ReportSuiteEvars evars : methods.getEvars(args)) {
-			System.out.println(String.format("Evars for %s (%s)", evars.getRsid(), evars.getSiteTitle()));
-			for (Evar e : evars.getEvars()) {
-				System.out.println(e);
-			}
+
+		if (args.length > 0) {
+			displayEvars(args, methods);
+			displayProps(args, methods);
+		} else {
+			displayReportSuites(methods);
+		}
+	}
+
+	private static void displayReportSuites(ReportSuiteMethods methods) throws IOException {
+		System.out.println("Loading report suite list...");
+		for (CompanyReportSuite report : methods.getReportSuites().getReportSuites()) {
+			System.out.println(report.getRsid());
 		}
 		System.out.println();
+	}
+
+	private static void displayProps(String[] args, final ReportSuiteMethods methods) throws IOException {
+		System.out.println("Loading props...");
 		for (ReportSuiteProps props : methods.getProps(args)) {
 			System.out.println(String.format("Props for %s (%s)", props.getRsid(), props.getSiteTitle()));
 			for (Prop p : props.getProps()) {
 				System.out.println(p);
 			}
 		}
+		System.out.println();
+	}
 
+	private static void displayEvars(String[] args, final ReportSuiteMethods methods) throws IOException {
+		System.out.println("Loading evars...");
+		for (ReportSuiteEvars evars : methods.getEvars(args)) {
+			System.out.println(String.format("Evars for %s (%s)", evars.getRsid(), evars.getSiteTitle()));
+			for (Evar e : evars.getEvars()) {
+				System.out.println(e);
+			}
+		}
+	}
+
+	private static Properties loadProperties() throws IOException {
+		final Properties properties = new Properties();
+		try (final FileInputStream fis = new FileInputStream(new File("analytics.properties"))) {
+			properties.load(fis);
+		} catch (FileNotFoundException e) {
+			System.err.println("Create the analytics.properties file");
+		}
+		return properties;
 	}
 }
