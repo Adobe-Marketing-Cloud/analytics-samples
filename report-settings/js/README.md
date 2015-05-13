57198e13a75b7b089556e9a63e52f04f09084484
Tomasz Rękawek <trekawek@gmail.com>
2015-05-13 10:46:14 +0200

    Refactor & clean-up.

--- report-settings/js/README.md
+++ report-settings/js/README.md
@@ -1,25 +1,26 @@
 ## Sample: Using APIs to audit report suite settings and show what has changed
 
-This sample shows how to gather config report suite data from Adobe Analytics using REST API.
+This sample shows how to gather config report suite data from the Adobe Analytics using REST API.
 
 Sample relies on:
-wsse.js - source: https://github.com/vrruiz/wsse-js/blob/master/wsse.js
 
-cdn jQuery lib - source: https://code.jquery.com/jquery-1.11.3.min.js
-
-cdn bootstrap css - source: https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css
+* [wsse.js](https://github.com/vrruiz/wsse-js)
+* [jQuery](https://jquery.com/)
+* [Bootstrap](http://getbootstrap.com/)
+* [Object diff](https://github.com/NV/objectDiff.js)
 
 ## Requirements 
-Browser with enabled JavaScript.
-Browser need to support FileReader() in JavaScript.
 
-## Installation
-To run sample just open index.html in Browser
+* Browser with enabled JavaScript and `FileReader()` support
 
+## Launching
 
-## Usage 
-1) Fill form with your credentials 
-2) Choose report suite
-3) Download json file with configuration
+Just open `index.html` in the browser
 
-If you upload file before retrieving report suite configuration then json comparision is performed. 
+## Usage
+
+1. Fill form with your credentials 
+2. Choose report suite
+3. Download json file with configuration
+
+If you upload file before retrieving report suite configuration, the application will compare the file with the current settings.
---
+++ report-settings/js/js/main.js
@@ -0,0 +1,65 @@
+(function ($, MarketingCloud) {
+    var METHODS = ["ReportSuite.GetEvars", "ReportSuite.GetProps"],
+        oldConfig = null;
+  
+    function fillReportsSelect() {
+        getAnalyticsClient().makeRequest('Company.GetReportSuites', '', function reportSuitesPopulate(data) {
+            var $select = $('#reportSuiteSelect');
+            $.each(data.report_suites, function() {
+                var $option = $('<option>', {
+                    value: this.rsid,
+                    text: this.site_title
+                });
+                $select.append($option);
+            });
+            $("#reportSuites").fadeIn(500);
+        });
+    }
+
+    function getReportSuiteConfiguration() {
+        var rsid = $('#reportSuiteSelect').val(),
+            requests,
+            analyticsClient = getAnalyticsClient();
+        requests = $.map(METHODS, function(method) {
+            return analyticsClient.makeRequest(method, { rsid_list : [rsid] });
+        });
+        $.when.apply($, requests).done(function() {
+            var results = arguments,
+                newConfig = {};
+            $.each(METHODS, function(i) {
+                newConfig[this.replace('.', '')] = results[i][0];
+            });
+            if (oldConfig) {
+                displayDifferences(newConfig);
+            } else {
+                saveConfiguration(newConfig);
+            }
+        });
+    }
+
+    function saveConfiguration(currentConfig) {
+        var filename = $("#reportSuiteSelect").val() + Date.now() + ".json";
+        MarketingCloud.fileSupport.writeFile(JSON.stringify(currentConfig, null, 2), filename, "application/json");
+    }
+
+    function displayDifferences(currentConfig) {
+        var diff = objectDiff.diffOwnProperties(oldConfig, currentConfig);
+		$('#checkResults').html(objectDiff.convertToXMLString(diff));
+    }
+
+    function getAnalyticsClient() {
+        return MarketingCloud.getAnalyticsClient($("#login").val(), $("#secret").val(), $("#endPoint").val());
+    }
+
+    $(function () {
+        $('#getReportSuites').click(fillReportsSelect);
+        $('#getReportSuiteConfiguration').click(getReportSuiteConfiguration);
+        $('#fileInput').change(function(e) {
+            MarketingCloud.fileSupport.readFile(this, function(contents) {
+                oldConfig = JSON.parse(contents);
+            }, function(err) {
+                alert(err);
+            })
+        });
+    });
+})(jQuery, window.MarketingCloud);
